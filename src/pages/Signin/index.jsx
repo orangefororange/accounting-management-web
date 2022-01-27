67eb551ccd8a10a6cf9708abef21abea8617e20a
orange for orange <showmeorange@gmail.com>
2022-01-27 22:17:58 +0800

    不知道module传不传上去

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
 export default class Signin extends Component {
   render() {
     return (
     <div>
         <h1>登录页面</h1>
+        <Button type="primary">Primary Button</Button>
     </div>);
   }
 }
